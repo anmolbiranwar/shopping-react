9b20b9198a26d2a42ca886f63522a5cad30d3645
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-11 10:38:43 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {LoginComponent} from './components/login/login.component';
+import { NetflixIndex } from './netflix/netflix-index/netflix-index';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NetflixIndex />
   </React.StrictMode>
 );
 
