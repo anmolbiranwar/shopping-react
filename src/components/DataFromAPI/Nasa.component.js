6326f23b360cb9431e849c6b2ed8a4da3add7b9c
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-15 11:42:29 -0700

    Update Nasa.component.js

--- src/components/DataFromAPI/Nasa.component.js
+++ src/components/DataFromAPI/Nasa.component.js
@@ -1,8 +1,22 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export function NasaComponent()
 {
+    const[mars, setMars]= useState([]);
 
+    function LoadPhotos(){
+        fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY")
+        .then(function(responese){
+            return responese.json();
+        })
+        .then(function(data){
+            setMars(data.photos);
+        })
+    }
+
+    useEffect(()=>{
+        LoadPhotos();
+    },[]);
 
     return(
         <div className="container-fluid mt-3">
