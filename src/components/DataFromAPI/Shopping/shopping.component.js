2392fc8f3c645f9652e83a220cff0b38119315e0
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-15 14:36:14 -0700

    Update shopping.component.js

--- src/components/DataFromAPI/Shopping/shopping.component.js
+++ src/components/DataFromAPI/Shopping/shopping.component.js
@@ -3,7 +3,17 @@ import axios from "axios";
 export function ShoppingComponent()
 {
     const[categories, setCategories]=useState([]);
-    
+
+    function LoadCategories(){
+        axios.get("https://fakestoreapi.com/products/categories")
+        .then(response=>{
+            setCategories(response.data);
+        })
+    }
+
+    useEffect(()=>{
+        LoadCategories();
+    },[]);
     return(
         <div className="container-fluid">
             <header className="bg-dark text-white text-center p-2">
@@ -15,7 +25,13 @@ export function ShoppingComponent()
                         <label className="form-label">Select Category</label>
                         <div>
                             <select className="form-select">
-
+                                {
+                                    categories.map(category=>
+                                        <option key={category} value={category}>
+                                            {category.toUpperCase()}
+                                        </option>
+                                        )
+                                }
                             </select>
                         </div>
                     </div>
