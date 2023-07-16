ad53a20e8ea260892a109a51c57dc794f8eb574f
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-16 12:29:52 -0700

    Update styledemo1.component.js

--- src/components/StyleBinding/styledemo1.component.js
+++ src/components/StyleBinding/styledemo1.component.js
@@ -1,28 +1,43 @@
+import { useState } from "react"
 
 export function StyleDemo1Component()
 {
+    const [styleObject, setStyleObject]= useState({backgroundColor:'', textAlign:''});
+
+    function backgroundChange(e){
+        setStyleObject({
+            backgroundColor:e.target.value,
+            textAlign:styleObject.textAlign
+        });
+    }
+    function alignChange(e){
+        setStyleObject({
+            textAlign:e.target.value,
+            backgroundColor:styleObject.backgroundColor
+        });
+    }
     return(
         <div className="contianer-fluid">
             <h2>Choose Style</h2>
             <dl>
                 <dt>Background Color</dt>
                 <dd>
-                    <select>
-                        <option>Yellow</option>
-                        <option>Green</option>
-                        <option>Red</option>
+                    <select onChange={backgroundChange}>
+                        <option value="yellow">Yellow</option>
+                        <option value="green">Green</option>
+                        <option value="red">Red</option>
                     </select>
                 </dd>
                 <dt>Text Align</dt>
                 <dd>
-                    <select>
-                        <option>Left</option>
-                        <option>Center</option>
-                        <option>Right</option>
+                    <select onChange={alignChange}>
+                        <option value="left">Left</option>
+                        <option value="center">Center</option>
+                        <option value="right">Right</option>
                     </select>
                 </dd>
             </dl>
-            <h1>Sample Text</h1>
+            <h1 style={styleObject}>Sample Text</h1>
         </div>
     )
 }
