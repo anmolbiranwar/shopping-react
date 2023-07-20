9145be4a17b245a576e02f69e039b6e0d7bdd37b
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-19 22:26:29 -0700

    Update formik-validation.component.js

--- src/components/Form Validation/formik-validation.component.js
+++ src/components/Form Validation/formik-validation.component.js
@@ -1,6 +1,28 @@
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 export function FormikValidation()
 {
+    const formik=useFormik({
+        initialValues:{
+            UserName:"",
+            Age:0,
+            Mobile:""
+        },
+        validationSchema: yup.object({
+            UserName: yup.string()
+            .required("User Name Required")
+            .min(4,"Name too short min 4 chars")
+            .max(10,"Name too long max 10 chars"),
+            Age: yup.number()
+            .required("Age Required"),
+            Mobile: yup.string()
+            .required("Mobile Required")
+            .matches(/\+91\d{10}/,"Invalid Mobile +91 and 10 digits")
+            
+
+        })
+    })
     return(
         <div className="container-fluid">
             <h2>Register</h2>
