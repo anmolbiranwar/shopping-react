54bc8a2d207dfd5a2d48491646e4d45b968843bf
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-07-19 22:17:49 -0700

    Update formik-validation.component.js

--- src/components/Form Validation/formik-validation.component.js
+++ src/components/Form Validation/formik-validation.component.js
@@ -3,7 +3,18 @@ export function FormikValidation()
 {
     return(
         <div className="container-fluid">
-            <h1>Formik Validation</h1>
+            <h2>Register</h2>
+        <form>
+            <dl>
+                <dt>User Name</dt>
+                <dd><input type="text"/></dd>
+                <dt>Age</dt>
+                <dd><input type="text"/></dd>
+                <dt>Mobile</dt>
+                <dd><input type="text"/></dd>
+            </dl>
+            <button>Register</button>
+        </form>
         </div>
     )
 }
